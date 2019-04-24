82d79fd7bf71d324ee40f5f5e8d127de2ad1e6a2
Skye Leahy <skyeleahy@protonmail.com>
2019-04-23 23:15:42 -0700

    first push to heroku

--- src/app.js
+++ src/app.js
@@ -22,14 +22,14 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather",
-    name: "Andrew Mead"
+    name: "Skye Leahy"
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
     title: "About Me",
-    name: "Andrew Mead"
+    name: "Skye Leahy"
   });
 });
 
@@ -37,7 +37,7 @@ app.get("/help", (req, res) => {
   res.render("help", {
     helpText: "This is some helpful text.",
     title: "Help",
-    name: "Andrew Mead"
+    name: "Skye Leahy"
   });
 });
 
@@ -86,7 +86,7 @@ app.get("/products", (req, res) => {
 app.get("/help/*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Skye Leahy",
     errorMessage: "Help article not found."
   });
 });
@@ -94,7 +94,7 @@ app.get("/help/*", (req, res) => {
 app.get("*", (req, res) => {
   res.render("404", {
     title: "404",
-    name: "Andrew Mead",
+    name: "Skye Leahy",
     errorMessage: "Page not found."
   });
 });
