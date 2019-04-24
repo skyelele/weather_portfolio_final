f26268cd1daff7ac12e2a3456c08a77ef4257788
Skye Leahy <skyeleahy@protonmail.com>
2019-04-23 23:21:08 -0700

    2nd heroku push

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath);
 
+const port = process.env.PORT || 3000;
+
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
@@ -99,6 +101,6 @@ app.get("*", (req, res) => {
   });
 });
 
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("Server is up on port 3000.");
 });
